97e82c50e9cef5b27e94d3168579457cc51284a8
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-19 17:29:03 +0700

    adding result components

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from "react";
 
 import { faker } from "@faker-js/faker";
 import RestartButton from "./components/RestartButton";
+import Result from "./components/Result";
 
 const words = faker.random.words(10);
 
 function App() {
   return (
     <>
-      <CountDownTImer timeLeft="30"/>
+      <CountDownTImer timeLeft="30" />
       <GenerateWords words={words} />
       <RestartButton
-      className={'mx-auto mt-10 text-slate-500'}
-      onRestart={() => null}
+        className={"mx-auto mt-10 text-slate-500"}
+        onRestart={() => null}
+      />
+      <Result
+       accuracyPercentage={10} 
+      errors={5}
+      total={100}
+      className="mt-10"
       />
     </>
   );
@@ -23,7 +30,7 @@ const GenerateWords = ({ words }: { words: string }) => {
   return <div className="text-4xl text-slate-500 text-center">{words}</div>;
 };
 
-const CountDownTImer = ({timeLeft}: {timeLeft: string}) => {
+const CountDownTImer = ({ timeLeft }: { timeLeft: string }) => {
   return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
 };
 
---
+++ src/components/Result.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Result({
+  errors,
+  accuracyPercentage,
+  total,
+  className,
+}: {
+  errors: number;
+  accuracyPercentage: number;
+  total: number;
+  className: string;
+}) {
+  return (
+    <ul
+      className={`flex flex-col items-center text-primary-400 space-y-3 ${className}`}
+    >
+      <li className="text-xl font-semibold">Results</li>
+      <li>Accuracy: {accuracyPercentage}</li>
+      <li className="text-red-500">Errors : {errors}</li>
+      <li>Typed: {total}</li>
+    </ul>
+  );
+}
+
+export default Result;
