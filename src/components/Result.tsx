c3afd76cb0ea31d6a19add52c22724d03d2a4c2e
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-19 17:43:19 +0700

    adding Animation to result components

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Result({
   errors,
@@ -11,15 +12,48 @@ function Result({
   total: number;
   className: string;
 }) {
+  const initial = { opacity: 0 };
+  const animate = { opacity: 1 };
+  const duration = { duration: 0.3 };
+
   return (
-    <ul
+    <motion.ul
       className={`flex flex-col items-center text-primary-400 space-y-3 ${className}`}
     >
-      <li className="text-xl font-semibold">Results</li>
-      <li>Accuracy: {accuracyPercentage}</li>
-      <li className="text-red-500">Errors : {errors}</li>
-      <li>Typed: {total}</li>
-    </ul>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        className="text-xl font-semibold"
+        transition={{ ...duration, delay: 0 }}
+      >
+        Results
+      </motion.li>
+
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 0.5 }}
+      >
+        Accuracy: {accuracyPercentage}
+      </motion.li>
+
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 1 }}
+        className="text-red-500"
+      >
+        Errors : {errors}
+      </motion.li>
+
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 1.4 }}
+      >
+        Typed: {total}
+      </motion.li>
+    </motion.ul>
   );
 }
 
