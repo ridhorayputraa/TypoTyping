d68ff5d90f7785618e398ef52837d741ed532d59
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-19 16:23:29 +0700

    create count timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import {faker} from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 const words = faker.random.words(10);
 
-// words adalah type words yang berbentuk string 
-const GenerateWords = ({words}:{words:string}) =>{
-  return (
-  <div className="text-4xl text-slate-500 text-center">
-     {words}     
-    </div>
-  )
-}
-
 function App() {
   return (
-<GenerateWords words={words}/>
+    <>
+      <CountDownTImer timeLeft="30"/>
+      <GenerateWords words={words} />
+    </>
   );
 }
+
+// words adalah type words yang berbentuk string
+const GenerateWords = ({ words }: { words: string }) => {
+  return <div className="text-4xl text-slate-500 text-center">{words}</div>;
+};
+
+const CountDownTImer = ({timeLeft}: {timeLeft: string}) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
+};
 
 export default App;
