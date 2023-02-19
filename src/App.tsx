8d40a0ff0d79854320648c642894c7f90bff50e1
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-19 16:11:08 +0700

    generating words using fakerjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import {faker} from '@faker-js/faker';
+
+const words = faker.random.words(10);
+
+// words adalah type words yang berbentuk string 
+const GenerateWords = ({words}:{words:string}) =>{
+  return (
+  <div className="text-4xl text-slate-500 text-center">
+     {words}     
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="text-4xl text-primary-500 text-center">
-     it's work      
-    </div>
+<GenerateWords words={words}/>
   );
 }
 
