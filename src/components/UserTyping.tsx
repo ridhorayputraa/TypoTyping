12b90d08c446d50fbf90d1f7843d396318781d9d
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-19 20:58:47 +0700

    create userTyping

---
+++ src/components/UserTyping.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function UserTyping({
+  userInput,
+  className,
+}: {
+  userInput: string;
+  className?: string;
+}) {
+  const typedCharacters = userInput.split("");
+
+  return (
+    <div>
+      {typedCharacters.map((char, index) => {
+        return <Character key={`${char}_${index}`} char={char}/>
+      })}
+    </div>
+
+  );
+}
+
+const Character = ({char}: {char:String}) => {
+    // warna text akan serprti tiap element dialog
+    return <span className="text-primary-400">{char}</span>
+} 
+
+export default UserTyping;
